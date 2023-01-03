3128ed4438c9d8d838bbe8844a2f2c16f274588c
haleytoye <hjtoye@gmail.com>
2023-01-03 14:26:10 -0500

    Added js to get local times via moment

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,26 @@
+setInterval(function () {
+  let sfElement = document.querySelector("#san-francisco");
+  let sfDateEl = sfElement.querySelector(".date");
+  let sfTimeEl = sfElement.querySelector(".time");
+  let sfTime = moment().tz("America/Los_Angeles");
+  sfDateEl.innerHTML = moment().format("dddd MMMM Do YYYY");
+  sfTimeEl.innerHTML = sfTime.format("H:mm:ss [<small>]A[</small>]");
+}, 1000);
 
+function updateParisTime() {
+  let prElement = document.querySelector("#paris");
+  let prDateEl = prElement.querySelector(".date");
+  let prTimeEl = prElement.querySelector(".time");
+  let prTime = moment().tz("Europe/Paris");
+  prDateEl.innerHTML = moment().format("dddd MMMM Do YYYY");
+  prTimeEl.innerHTML = prTime.format("H:mm:ss [<small>]A[</small>]");
+
+  let isElement = document.querySelector("#istanbul");
+  let isDateEl = isElement.querySelector(".date");
+  let isTimeEl = isElement.querySelector(".time");
+  let isTime = moment().tz("Europe/Istanbul");
+  isDateEl.innerHTML = moment().format("dddd MMMM Do YYYY");
+  isTimeEl.innerHTML = isTime.format("H:mm:ss [<small>]A[</small>]");
+}
+updateParisTime();
+setInterval(updateParisTime, 1000);
