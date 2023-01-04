c52fe2ace0bea0b3134137f2479f82bbc58e34c6
haleytoye <hjtoye@gmail.com>
2023-01-03 19:45:05 -0500

    update to include current location

--- javascript/index.js
+++ javascript/index.js
@@ -37,6 +37,9 @@ setInterval(updateParisTime, 1000);
 
 function updateCity(event) {
   let cityTZ = event.target.value;
+  if (cityTZ === "current") {
+    cityTZ = moment.tz.guess();
+  }
   let cityName = cityTZ.replace("_", "").split("/")[1];
   let cityTime = moment().tz(cityTZ);
   let citiesEl = document.querySelector("#cities");
